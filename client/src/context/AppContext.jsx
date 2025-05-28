27d6fcfb2403fc284ffda4e9d5807667366487f3
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-05-28 21:51:11 +0530

    feat: set up AppContext for global state management and update main rendering logic

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+  const value = { navigate, user, setUser, isSeller, setIsSeller };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
