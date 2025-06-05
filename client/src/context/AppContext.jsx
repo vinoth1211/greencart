8dc975b9aab491cf6ffdec6ab643d77e2c249787
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-05 21:43:28 +0530

    feat: enhance ProductCard and AppContext for dynamic product display and currency support

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,13 +1,25 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+  const currency = import.meta.VITE_CURRENCY;
+
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
   const [isSeller, setIsSeller] = useState(false);
   const [showUserLogin, setShowUserLogin] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    setProducts(dummyProducts);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   const value = {
     navigate,
@@ -17,6 +29,8 @@ export const AppContextProvider = ({ children }) => {
     setIsSeller,
     showUserLogin,
     setShowUserLogin,
+    products,
+    currency,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
