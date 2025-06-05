74345fba8734fb10a1d3c9cec511b5549c6dffbc
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-05 21:55:08 +0530

    feat: integrate toast notifications for cart actions and enhance AppContext with cart management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -9,6 +10,9 @@ const App = () => {
   return (
     <div>
       {isSellerPath ? null : <Navbar />}
+
+      <Toaster />
+
       <div
         className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}
       >
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
@@ -13,8 +14,24 @@ export const AppContextProvider = ({ children }) => {
   const [showUserLogin, setShowUserLogin] = useState(false);
   const [products, setProducts] = useState([]);
 
+  const [cartItems, setCartItems] = useState({});
+
+  // Fetch All Products
   const fetchProducts = async () => {
     setProducts(dummyProducts);
+  };
+
+  // Add Product to Cart
+  const addToCart = () => {
+    let cartData = structuredClone(cartItems);
+
+    if (cartData[itemId]) {
+      cartData[itemId] += 1;
+    } else {
+      cartData[itemId] = 1;
+    }
+    setCartItems(cartData);
+    toast.success("Added to Cart");
   };
 
   useEffect(() => {
@@ -31,6 +48,7 @@ export const AppContextProvider = ({ children }) => {
     setShowUserLogin,
     products,
     currency,
+    addToCart,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
