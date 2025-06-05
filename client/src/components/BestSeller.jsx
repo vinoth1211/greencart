9bcd42b501e3b364f3204048cd6a6c89d05803d8
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-05 21:26:37 +0530

    feat: add ProductCard component and integrate it into BestSeller component

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import ProductCard from './ProductCard'
 
 const BestSeller = () => {
   return (
     <div className='mt-16'>
         <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
         <div>
-
+          <ProductCard />
         </div>
     </div>
   )
