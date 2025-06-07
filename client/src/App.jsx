94f26b8e1ce6bd0f21261ac8af1d74df355a4fe6
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-07 21:27:42 +0530

    feat: add AllProducts page with product filtering based on search query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer";
 import { useAppContext } from "./context/AppContext";
 import Login from "./components/Login";
+import AllProducts from "./pages/AllProducts";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
@@ -23,6 +24,7 @@ const App = () => {
       >
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/products" element={<AllProducts />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -15,6 +15,7 @@ export const AppContextProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
 
   const [cartItems, setCartItems] = useState({});
+  const [searchQuery, setSearchQuery] = useState({});
 
   // Fetch All Products
   const fetchProducts = async () => {
@@ -73,6 +74,8 @@ export const AppContextProvider = ({ children }) => {
     updateCartItem,
     removeFromCart,
     cartItems,
+    searchQuery,
+    setSearchQuery,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
