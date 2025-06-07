93391dd64f12c241252224237db8b643eafdfb5a
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-07 21:50:53 +0530

    feat: update ProductCategory to display filtered products and handle empty states

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useAppContext } from "../context/AppContext";
 import { useParams } from "react-router-dom";
 import { categories } from "../assets/assets";
+import ProductCard from "../components/ProductCard";
 
 const ProductCategory = () => {
   const { products } = useAppContext();
@@ -23,6 +24,19 @@ const ProductCategory = () => {
             {searchCategory.text.toUpperCase()}
           </p>
           <div className="w-16 h-0.5 bg-primary rounded-full"></div>
+        </div>
+      )}
+      {filteredProducts.length > 0 ? (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6">
+          {filteredProducts.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex items-center justify-center h-[60vh]">
+          <p className="text-2xl font-medium text-primary">
+            No products found in this category.
+          </p>
         </div>
       )}
     </div>
