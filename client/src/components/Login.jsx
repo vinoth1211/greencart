dd0c57038281a2b299dcf52c8a266165eeaea1f4
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-07 21:06:03 +0530

    feat: integrate AppContext into Login component for user login state management and update styling

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { useAppContext } from "../context/AppContext";
 
 const Login = () => {
-    
+  const { setShowUserLogin } = useAppContext();
+
   const [state, setState] = React.useState("login");
   const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
 
   return (
-    <div className="fixed top-0 bottom-0 left-0 right-0 z-30 flex items-center text-sm text-gray-600 bg-black/50">
-      <form className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] rounded-lg shadow-xl border border-gray-200 bg-white">
+    <div
+      onClick={() => setShowUserLogin(false)}
+      className="fixed top-0 bottom-0 left-0 right-0 z-30 flex items-center text-sm text-gray-600 bg-black/50"
+    >
+      <form
+        onClick={(e) => e.stopPropagation()}
+        className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] rounded-lg shadow-xl border border-gray-200 bg-white"
+      >
         <p className="text-2xl font-medium m-auto">
-          <span className="text-indigo-500">User</span>{" "}
+          <span className="text-primary">User</span>{" "}
           {state === "login" ? "Login" : "Sign Up"}
         </p>
         {state === "register" && (
@@ -21,7 +29,7 @@ const Login = () => {
               onChange={(e) => setName(e.target.value)}
               value={name}
               placeholder="type here"
-              className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500"
+              className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
               type="text"
               required
             />
@@ -33,7 +41,7 @@ const Login = () => {
             onChange={(e) => setEmail(e.target.value)}
             value={email}
             placeholder="type here"
-            className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500"
+            className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
             type="email"
             required
           />
@@ -44,7 +52,7 @@ const Login = () => {
             onChange={(e) => setPassword(e.target.value)}
             value={password}
             placeholder="type here"
-            className="border border-gray-200 rounded w-full p-2 mt-1 outline-indigo-500"
+            className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
             type="password"
             required
           />
@@ -54,7 +62,7 @@ const Login = () => {
             Already have account?{" "}
             <span
               onClick={() => setState("login")}
-              className="text-indigo-500 cursor-pointer"
+              className="text-primary cursor-pointer"
             >
               click here
             </span>
@@ -64,13 +72,13 @@ const Login = () => {
             Create an account?{" "}
             <span
               onClick={() => setState("register")}
-              className="text-indigo-500 cursor-pointer"
+              className="text-primary cursor-pointer"
             >
               click here
             </span>
           </p>
         )}
-        <button className="bg-indigo-500 hover:bg-indigo-600 transition-all text-white w-full py-2 rounded-md cursor-pointer">
+        <button className="bg-primary hover:bg-primary-dull transition-all text-white w-full py-2 rounded-md cursor-pointer">
           {state === "register" ? "Create Account" : "Login"}
         </button>
       </form>
