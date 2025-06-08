d04fe7f6baa45007a4db75fa923970b3624c112e
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-08 20:47:13 +0530

    feat: update Cart component styles and improve item removal functionality

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -44,9 +44,7 @@ const Cart = () => {
       <div className="flex-1 max-w-4xl">
         <h1 className="text-3xl font-medium mb-6">
           Shopping Cart{" "}
-          <span className="text-sm text-indigo-500">
-            {getCartCount()} Items
-          </span>
+          <span className="text-sm text-primary">{getCartCount()} Items</span>
         </h1>
 
         <div className="grid grid-cols-[2fr_1fr_1fr] text-gray-500 text-base font-medium pb-3">
@@ -110,7 +108,7 @@ const Cart = () => {
               {product.offerPrice * product.quantity}
             </p>
             <button
-              onClick={() => removeFromCart()}
+              onClick={() => removeFromCart(product._id)}
               className="cursor-pointer mx-auto"
             >
               <img
@@ -127,7 +125,7 @@ const Cart = () => {
             navigate("/products");
             scrollTo(0, 0);
           }}
-          className="group cursor-pointer flex items-center mt-8 gap-2 text-indigo-500 font-medium"
+          className="group cursor-pointer flex items-center mt-8 gap-2 text-primary font-medium"
         >
           <img
             src={assets.arrow_right_icon_colored}
@@ -152,7 +150,7 @@ const Cart = () => {
             </p>
             <button
               onClick={() => setShowAddress(!showAddress)}
-              className="text-indigo-500 hover:underline cursor-pointer"
+              className="text-primary hover:underline cursor-pointer"
             >
               Change
             </button>
@@ -172,7 +170,7 @@ const Cart = () => {
                 ))}
                 <p
                   onClick={() => navigate("/add-address")}
-                  className="text-indigo-500 text-center cursor-pointer p-2 hover:bg-indigo-500/10"
+                  className="text-primary text-center cursor-pointer p-2 hover:bg-primary/10"
                 >
                   Add address
                 </p>
@@ -209,21 +207,21 @@ const Cart = () => {
             <span>Tax (2%)</span>
             <span>
               {currency}
-              {(getCartAmount() * 2) / 100}
+              {getCartAmount() * 2 / 100}
             </span>
           </p>
           <p className="flex justify-between text-lg font-medium mt-3">
             <span>Total Amount:</span>
             <span>
               {currency}
-              {getCartAmount() + (getCartAmount() * 2) / 100}
+              {getCartAmount() + getCartAmount() * 2 / 100}
             </span>
           </p>
         </div>
 
         <button
           onClick={placeOrder}
-          className="w-full py-3 mt-6 cursor-pointer bg-indigo-500 text-white font-medium hover:bg-indigo-600 transition"
+          className="w-full py-3 mt-6 cursor-pointer bg-primary text-white font-medium hover:bg-primary-dull transition"
         >
           {paymentOption === "COD" ? "Place Order" : "Proceed to Checkout"}
         </button>
