42f7dc64fcbacffb7432af138cc2dfc170c4d075
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-09 11:27:58 +0530

    feat: enhance AddAddress component with input handling and form structure

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -1,17 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
 
 // Input Field Component
-const InputField = () => <input type="text" />;
+const InputField = ({ type, placeholder, name, handleChange, address }) => (
+  <input
+    className="w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition"
+    type={type}
+    placeholder={placeholder}
+    onChange={handleChange}
+    name={name}
+    value={address[name]}
+    required
+  />
+);
 
 const AddAddress = () => {
+  const [address, setAddress] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipCode: "",
+    country: "",
+    phone: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setAddress((prevAddress) => ({
+      ...AddAddress,
+      [name]: value,
+    }));
+  };
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="mt-16 pb-16">
       <p className="text-2xl md:text-3xl text-gray-500">
         Add Shipping <span className="font-semibold text-primary">Address</span>
       </p>
       <div className="flex flex-col-reverse md:flex-row justify-between mt-10">
-        <div className="flex-1 max-w-md"></div>
+        <div className="flex-1 max-w-md">
+          <form onSubmit={onSubmitHandler} className="space-y-3 mt-6 text-sm">
+            <div>
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="firstName"
+                type="text"
+                placeholder="First Name"
+              />{" "}
+              <InputField
+                handleChange={handleChange}
+                address={address}
+                name="lasttName"
+                type="text"
+                placeholder="Last Name"
+              />
+            </div>
+          </form>
+        </div>
         <img
           className="md:mr-16 mb-16 md:mt-0"
           src={assets.add_address_iamge}
