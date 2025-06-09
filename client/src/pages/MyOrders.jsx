15332b8836ddc5dd577b10cd1aed57da66e3c378
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-09 11:55:09 +0530

    feat: add MyOrders page and integrate it into routing in App component

---
+++ client/src/pages/MyOrders.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MyOrders = () => {
+  return (
+    <div className="mt-16 pb-6">
+      <div className="flex flex-col items-end w-max mb-8">
+        <p className="text-2xl font-medium uppercase">My Orders</p>
+        <div className="w-16 h-0.5 bg-primary rounded-full"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MyOrders;
