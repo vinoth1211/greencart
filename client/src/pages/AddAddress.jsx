da98f0d6285a4a8ff402b036698dc99084ca6529
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-09 11:14:13 +0530

    feat: refactor AddAddress component for improved readability and consistency

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 // Input Field Component
-const InputField = (()=>(
-    <input type="text" />
-)) 
+const InputField = () => <input type="text" />;
 
 const AddAddress = () => {
   return (
-    <div className='mt-16 pb-16'>
-        <p className='text-2xl md:text-3xl text-gray-500'>Add Shipping <span className='font-semibold text-primary'>Address</span></p>
+    <div className="mt-16 pb-16">
+      <p className="text-2xl md:text-3xl text-gray-500">
+        Add Shipping <span className="font-semibold text-primary">Address</span>
+      </p>
+      <div className="flex flex-col-reverse md:flex-row justify-between mt-10">
+        <div className="flex-1 max-w-md"></div>
+        <img
+          className="md:mr-16 mb-16 md:mt-0"
+          src={assets.add_address_iamge}
+          alt="Add Address"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddAddress
+export default AddAddress;
