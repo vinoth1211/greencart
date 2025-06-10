0cae7ddb5d4a0c0c996ed9bcacc1585e241d550b
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-10 20:38:51 +0530

    feat: refactor Orders component for improved structure and layout

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -1,60 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAppContext } from "../../context/AppContext";
+import { assets, dummyOrders } from "../../assets/assets";
 
 const Orders = () => {
+  const { currency } = useAppContext();
+  const [orders, setOrders] = useState([]);
+
+  const fetchOrders = async () => {
+    setOrders(dummyOrders);
+  };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
   return (
-    <div className="md:p-10 p-4 space-y-4">
-      <h2 className="text-lg font-medium">Orders List</h2>
-      {orders.map((order, index) => (
-        <div
-          key={index}
-          className="flex flex-col md:grid md:grid-cols-[2fr_1fr_1fr_1fr] md:items-center gap-5 p-5 max-w-4xl rounded-md border border-gray-300 text-gray-800"
-        >
-          <div className="flex gap-5">
-            <img
-              className="w-12 h-12 object-cover opacity-60"
-              src={boxIcon}
-              alt="boxIcon"
-            />
-            <>
-              {order.items.map((item, index) => (
-                <div key={index} className="flex flex-col justify-center">
-                  <p className="font-medium">
-                    {item.product.name}{" "}
-                    <span
-                      className={`text-indigo-500 ${
-                        item.quantity < 2 && "hidden"
-                      }`}
-                    >
-                      x {item.quantity}
-                    </span>
-                  </p>
-                </div>
-              ))}
-            </>
-          </div>
+    <div className="no-scrollbar flex-1 h-[95vh] overflow-y-scroll">
+      <div className="md:p-10 p-4 space-y-4">
+        <h2 className="text-lg font-medium">Orders List</h2>
+        {orders.map((order, index) => (
+          <div
+            key={index}
+            className="flex flex-col md:items-center md:flex-row gap-5 justify-betweenp-5 max-w-4xl rounded-md border border-gray-300"
+          >
+            <div className="flex gap-5 max-w-80">
+              <img
+                className="w-12 h-12 object-cover"
+                src={assets.box_icon}
+                alt="boxIcon"
+              />
+              <>
+                {order.items.map((item, index) => (
+                  <div key={index} className="flex flex-col">
+                    <p className="font-medium">
+                      {item.product.name}
+                      <span className="text-primary">x {item.quantity}</span>
+                    </p>
+                  </div>
+                ))}
+              </>
+            </div>
 
-          <div className="text-sm">
-            <p className="font-medium mb-1">
-              {order.address.firstName} {order.address.lastName}
+            <div className="text-sm md:text-base text-black/60">
+              <p className="text-black/80">
+                {order.address.firstName} {order.address.lastName}
+              </p>
+              <p>
+                {order.address.street}, {order.address.city}
+              </p>
+              <p>
+                {order.address.state},{order.address.zipcode},
+                {order.address.country}
+              </p>
+              <p></p>
+              <p>{order.address.phone}</p>
+            </div>
+
+            <p className="font-medium text-lg my-auto">
+              {currency}
+              {order.amount}
             </p>
-            <p>
-              {order.address.street}, {order.address.city},{" "}
-              {order.address.state},{order.address.zipcode},{" "}
-              {order.address.country}
-            </p>
-          </div>
 
-          <p className="font-medium text-base my-auto text-black/70">
-            ${order.amount}
-          </p>
-
-          <div className="flex flex-col text-sm">
-            <p>Method: {order.paymentType}</p>
-            <p>Date: {order.orderDate}</p>
-            <p>Payment: {order.isPaid ? "Paid" : "Pending"}</p>
+            <div className="flex flex-col text-sm md:text-base text-black/60">
+              <p>Method: {order.paymentType}</p>
+              <p>Date: {new Date(order.createAt).toLocaleDateString()}</p>
+              <p>Payment: {order.isPaid ? "Paid" : "Pending"}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
