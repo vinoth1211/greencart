1ed7c805568338b78aab9b1f1da54651673cfbda
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-10 20:23:36 +0530

    feat: enhance Orders component with structured order display and improved layout

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React from "react";
 
 const Orders = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <div className="md:p-10 p-4 space-y-4">
+      <h2 className="text-lg font-medium">Orders List</h2>
+      {orders.map((order, index) => (
+        <div
+          key={index}
+          className="flex flex-col md:grid md:grid-cols-[2fr_1fr_1fr_1fr] md:items-center gap-5 p-5 max-w-4xl rounded-md border border-gray-300 text-gray-800"
+        >
+          <div className="flex gap-5">
+            <img
+              className="w-12 h-12 object-cover opacity-60"
+              src={boxIcon}
+              alt="boxIcon"
+            />
+            <>
+              {order.items.map((item, index) => (
+                <div key={index} className="flex flex-col justify-center">
+                  <p className="font-medium">
+                    {item.product.name}{" "}
+                    <span
+                      className={`text-indigo-500 ${
+                        item.quantity < 2 && "hidden"
+                      }`}
+                    >
+                      x {item.quantity}
+                    </span>
+                  </p>
+                </div>
+              ))}
+            </>
+          </div>
 
-export default Orders
+          <div className="text-sm">
+            <p className="font-medium mb-1">
+              {order.address.firstName} {order.address.lastName}
+            </p>
+            <p>
+              {order.address.street}, {order.address.city},{" "}
+              {order.address.state},{order.address.zipcode},{" "}
+              {order.address.country}
+            </p>
+          </div>
+
+          <p className="font-medium text-base my-auto text-black/70">
+            ${order.amount}
+          </p>
+
+          <div className="flex flex-col text-sm">
+            <p>Method: {order.paymentType}</p>
+            <p>Date: {order.orderDate}</p>
+            <p>Payment: {order.isPaid ? "Paid" : "Pending"}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Orders;
