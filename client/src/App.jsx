6a001aebb909cc5974ef4da3badcd97ef520ec7a
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-10 09:08:48 +0530

    feat: add SellerLogin component and integrate it into routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,10 +12,11 @@ import ProductDetails from "./pages/ProductDetails";
 import Cart from "./pages/Cart";
 import AddAddress from "./pages/AddAddress";
 import MyOrders from "./pages/MyOrders";
+import SellerLogin from "./components/seller/SellerLogin";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
-  const { showUserLogin } = useAppContext();
+  const { showUserLogin, isSeller } = useAppContext();
 
   return (
     <div>
@@ -35,6 +36,9 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/add-address" element={<AddAddress />} />
           <Route path="/my-orders" element={<MyOrders />} />
+          <Route path="/seller" element={isSeller ? null : <SellerLogin />}>
+
+          </Route>
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
