3019f4fecffd2191331fc6939cac9f26350e57c4
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-10 20:14:40 +0530

    style: update button color in AddProduct component for consistency

--- client/src/pages/seller/AddProduct.jsx
+++ client/src/pages/seller/AddProduct.jsx
@@ -129,7 +129,7 @@ const AddProduct = () => {
             />
           </div>
         </div>
-        <button className="px-8 py-2.5 bg-indigo-500 text-white font-medium rounded">
+        <button className="px-8 py-2.5 bg-primary text-white font-medium rounded cursor-pointer">
           ADD
         </button>
       </form>
