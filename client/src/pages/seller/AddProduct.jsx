0817426fcf9467581b1488a389f44f8176a0c5a2
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-10 19:57:25 +0530

    feat: implement AddProduct form with image upload and product details input

--- client/src/pages/seller/AddProduct.jsx
+++ client/src/pages/seller/AddProduct.jsx
@@ -1,11 +1,111 @@
-import React from 'react'
+import React from "react";
 
 const AddProduct = () => {
   return (
-    <div>
-        
+    <div className="py-10 flex flex-col justify-between bg-white">
+      <form className="md:p-10 p-4 space-y-5 max-w-lg">
+        <div>
+          <p className="text-base font-medium">Product Image</p>
+          <div className="flex flex-wrap items-center gap-3 mt-2">
+            {Array(4)
+              .fill("")
+              .map((_, index) => (
+                <label key={index} htmlFor={`image${index}`}>
+                  <input
+                    accept="image/*"
+                    type="file"
+                    id={`image${index}`}
+                    hidden
+                  />
+                  <img
+                    className="max-w-24 cursor-pointer"
+                    src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/e-commerce/uploadArea.png"
+                    alt="uploadArea"
+                    width={100}
+                    height={100}
+                  />
+                </label>
+              ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-1 max-w-md">
+          <label className="text-base font-medium" htmlFor="product-name">
+            Product Name
+          </label>
+          <input
+            id="product-name"
+            type="text"
+            placeholder="Type here"
+            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-1 max-w-md">
+          <label
+            className="text-base font-medium"
+            htmlFor="product-description"
+          >
+            Product Description
+          </label>
+          <textarea
+            id="product-description"
+            rows={4}
+            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40 resize-none"
+            placeholder="Type here"
+          ></textarea>
+        </div>
+        <div className="w-full flex flex-col gap-1">
+          <label className="text-base font-medium" htmlFor="category">
+            Category
+          </label>
+          <select
+            id="category"
+            className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+          >
+            <option value="">Select Category</option>
+            {[
+              { name: "Electronics" },
+              { name: "Clothing" },
+              { name: "Accessories" },
+            ].map((item, index) => (
+              <option key={index} value={item.name}>
+                {item.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="flex items-center gap-5 flex-wrap">
+          <div className="flex-1 flex flex-col gap-1 w-32">
+            <label className="text-base font-medium" htmlFor="product-price">
+              Product Price
+            </label>
+            <input
+              id="product-price"
+              type="number"
+              placeholder="0"
+              className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+              required
+            />
+          </div>
+          <div className="flex-1 flex flex-col gap-1 w-32">
+            <label className="text-base font-medium" htmlFor="offer-price">
+              Offer Price
+            </label>
+            <input
+              id="offer-price"
+              type="number"
+              placeholder="0"
+              className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40"
+              required
+            />
+          </div>
+        </div>
+        <button className="px-8 py-2.5 bg-indigo-500 text-white font-medium rounded">
+          ADD
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddProduct
+export default AddProduct;
