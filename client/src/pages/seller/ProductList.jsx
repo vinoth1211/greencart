8b08985c995919cdf0c8cc45465df2d872d4f41c
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-10 20:20:35 +0530

    feat: update ProductList component to utilize context for product data and improve rendering logic

--- client/src/pages/seller/ProductList.jsx
+++ client/src/pages/seller/ProductList.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { useAppContext } from "../../context/AppContext";
 
 const ProductList = () => {
+  const { products, currency } = useAppContext();
+
   return (
-    <div className="flex-1 py-10 flex flex-col justify-between">
+    <div className="no-scrollbar flex-1 h-[95vh] overflow-y-scroll flex flex-col justify-between">
       <div className="w-full md:p-10 p-4">
         <h2 className="pb-4 text-lg font-medium">All Products</h2>
         <div className="flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-500/20">
@@ -18,11 +21,15 @@ const ProductList = () => {
               </tr>
             </thead>
             <tbody className="text-sm text-gray-500">
-              {products.map((product, index) => (
-                <tr key={index} className="border-t border-gray-500/20">
+              {products.map((product) => (
+                <tr key={product._id} className="border-t border-gray-500/20">
                   <td className="md:px-4 pl-2 md:pl-4 py-3 flex items-center space-x-3 truncate">
                     <div className="border border-gray-300 rounded p-2">
-                      <img src={product.image} alt="Product" className="w-16" />
+                      <img
+                        src={product.image[0]}
+                        alt="Product"
+                        className="w-16"
+                      />
                     </div>
                     <span className="truncate max-sm:hidden w-full">
                       {product.name}
@@ -30,15 +37,12 @@ const ProductList = () => {
                   </td>
                   <td className="px-4 py-3">{product.category}</td>
                   <td className="px-4 py-3 max-sm:hidden">
-                    ${product.offerPrice}
+                    {currency}
+                    {product.offerPrice}
                   </td>
                   <td className="px-4 py-3">
                     <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
-                      <input
-                        type="checkbox"
-                        className="sr-only peer"
-                        defaultChecked={product.inStock}
-                      />
+                      <input type="checkbox" className="sr-only peer" />
                       <div className="w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600 transition-colors duration-200"></div>
                       <span className="dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5"></span>
                     </label>
