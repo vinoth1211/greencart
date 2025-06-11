0adc45da9320c3f47bfee1618c3809ed4fc59137
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-11 20:25:25 +0530

    feat: implement user registration endpoint with JWT authentication

---
+++ server/controllers/userController.js
@@ -0,0 +1,46 @@
+import User from "../models/user.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// Register User : /api/users/register
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.json({ success: false, message: "Missing Details" });
+    }
+
+    const exisitingUser = await User.findOne({ email });
+
+    if (exisitingUser)
+      return res.json({ success: false, message: "User Already Exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true, // Prevent Javascript access to the cookie
+      secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // CSRF protection
+      maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expiration time
+    });
+
+    return res.json({
+      success: true,
+      user: { email: user.email, name: user.name },
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
