f8669e4c18769336b7577600b97a8afeeec07e38
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-11 19:43:28 +0530

    feat: set up server with Express and initial route

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => res.send("API is Working"));
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
