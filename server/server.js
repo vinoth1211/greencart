c7e040dce4713f19cca4aa1b02e9df33e4d735cc
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-11 20:02:21 +0530

    feat: establish database connection and load environment variables in server setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
+import connectDB from "./configs/db.js";
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+await connectDB();
 
 //Allow multiple origins
 const allowedOrigins = ["http://localhost:5173"];
