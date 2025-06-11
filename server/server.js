f0b887e5b8ea4082d5c8b6dd1c8a1166601fcd47
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-11 20:33:23 +0530

    feat: add user route for registration endpoint

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import "dotenv/config";
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -18,6 +19,7 @@ app.use(cookieParser());
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => res.send("API is Working"));
+app.use("/api/user", userRouter)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
