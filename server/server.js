a5e11abf726927b94d5909f67e778ff20a6c0162
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-06-11 19:47:00 +0530

    feat: add CORS middleware to allow multiple origins

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+//Allow multiple origins
+const allowedOrigins = ["http://localhost:5173"];
+
+// Middleware configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => res.send("API is Working"));
 
